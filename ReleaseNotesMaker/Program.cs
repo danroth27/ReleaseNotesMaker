f7c70f69c3b7ef098ad3d17c18a6288dc84ff981
danroth27 <daroth@microsoft.com>
2014-10-05 01:12:27 -0700

    Use Octokit and support drafting releases.
    Migrated to use Octokit.
    Added support for OAuth tokens in addition to username and password.
    Added functionality to publish draft releases.
    Added functionality to query an entire organization for repos.

--- ReleaseNotesMaker/Program.cs
+++ ReleaseNotesMaker/Program.cs
@@ -8,30 +8,48 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Octokit;
 
 namespace ReleaseNotesMaker
 {
     class Program
     {
+        static GitHubClient client;
+
         static int Main(string[] args)
         {
-            var user = Environment.GetEnvironmentVariable("GITHUB_USER");
-            var password = Environment.GetEnvironmentVariable("GITHUB_PASSWORD");
-
-            if (args.Length < 2)
+            if (args.Length != 2)
             {
-                Console.WriteLine("Usage: [repoUrl] [milestone]");
-                Console.WriteLine("Sample:");
-                Console.WriteLine("{0}.exe signalr/signalr 2.1.1", typeof(Program).Assembly.GetName().Name);
+                DisplayHelp();
                 return 1;
             }
 
             string repoURL = args[0];
             string milestone = args[1];
 
+            string[] repoUrlSegments = repoURL.Split('/');
+            if (repoUrlSegments.Length > 2)
+            {
+                DisplayHelp();
+                return 1;
+            }
+
+            client = new GitHubClient(new ProductHeaderValue("ReleaseNotesMaker"));
+            AddClientCredentials(client);
+
+            string owner = repoUrlSegments[0];
+            string repoName = repoUrlSegments.Length > 1 ? repoUrlSegments[1] : null;
+
             try
             {
-                BuildReleaseNotesForMilestone(user, password, repoURL, milestone).Wait();
+                if (repoName != null)
+                {
+                    PublishReleaseForMilestone(owner, repoName, milestone).Wait();
+                }
+                else
+                {
+                    PublishReleasesForMilestone(owner, milestone).Wait();
+                }
             }
             catch (Exception ex)
             {
@@ -42,22 +60,105 @@ namespace ReleaseNotesMaker
             return 0;
         }
 
-        private static async Task BuildReleaseNotesForMilestone(string user, string password, string repoURL, string milestone)
+        private static void DisplayHelp()
         {
-            List<JToken> issues = await GetIssuesForMilestone(user, password, repoURL);
+            Console.WriteLine("Usage: [repoURL] [milestone]");
+            Console.WriteLine("Sample:");
+            Console.WriteLine("{0}.exe signalr/signalr 2.1.1", typeof(Program).Assembly.GetName().Name);
+        }
+
+        private static void AddClientCredentials(GitHubClient client)
+        {
+            var user = Environment.GetEnvironmentVariable("GITHUB_USER");
+            var password = Environment.GetEnvironmentVariable("GITHUB_PASSWORD");
+            var token = Environment.GetEnvironmentVariable("GITHUB_TOKEN");
+
+            if (token != null)
+            {
+                client.Credentials = new Credentials(token);
+            }
+            else if (user != null && password != null)
+            {
+                client.Credentials = new Credentials(user, password);
+            }
+        }
+
+        private static async Task PublishReleasesForMilestone(string org, string milestone)
+        {
+            var repos = await client.Repository.GetAllForOrg(org);
+            Console.WriteLine("Found ({0}) repositories in {1}", repos.Count, org);
+            foreach (var repo in repos)
+            {
+                await PublishReleaseForMilestone(repo.Owner.Login, repo.Name, milestone);
+            }
+        }
+
+        private static async Task PublishReleaseForMilestone(string owner, string repoName, string milestone)
+        {
+            Console.WriteLine("{0}/{1}", owner, repoName);
+
+            var milestones = await GetMilestonesForRepository(owner, repoName);
+            var matchingMilestones = milestones.Where(m => m.Title.EndsWith(milestone));
+
+            foreach (var matchingMilestone in matchingMilestones)
+            {
+                string releaseNotes = await BuildReleaseNotesForMilestone(owner, repoName, matchingMilestone);
+
+                if (ShouldCreateDraftRelease())
+                {
+                    await CreateDraftRelease(owner, repoName, matchingMilestone.Title, matchingMilestone.Title, "release", releaseNotes);
+                }
+            }
+        }
+
+        private static bool ShouldCreateDraftRelease()
+        {
+            Console.Write("Create draft release? [Y/N]: N");
+            Console.CursorLeft = Console.CursorLeft - 1;
+            string yesNo = Console.ReadLine();
+            return String.Compare(yesNo, "Y", true) == 0;
+        }
+
+        private static async Task CreateDraftRelease(string owner, string repoName, string tagName, string releaseName, string branch, string description)
+        {
+            var releaseUpdate = new ReleaseUpdate(tagName)
+            {
+                TargetCommitish = branch,
+                Name = releaseName,
+                Body = description,
+                Prerelease = true,
+                Draft = true
+            };
+
+            var release = await client.Release.Create(owner, repoName, releaseUpdate);
+        }
+
+        private static async Task<List<Milestone>> GetMilestonesForRepository(string owner, string repoName)
+        {
+            var openMilestones = await client.Issue.Milestone.GetForRepository(owner, repoName);
+            List<Milestone> milestones = new List<Milestone>(openMilestones);
+
+            var milestoneRequest = new MilestoneRequest() { State = ItemState.Closed };
+            var closedMilestones = await client.Issue.Milestone.GetForRepository(owner, repoName, milestoneRequest);
+            milestones.AddRange(closedMilestones);
+
+            return milestones;
+        }
+
+        private static async Task<string> BuildReleaseNotesForMilestone(string owner, string repoName, Milestone milestone)
+        {
+            var issues = await GetClosedIssuesInMilestone(owner, repoName, milestone);
 
             var issueGroups = from issue in issues
-                              let labels = (from label in issue["labels"]
-                                            select label.Value<string>("name")).ToList()
-                              let category = Categorize(labels)
-                              let issueMilestone = issue["milestone"]
-                              where labels.Count > 0 && issueMilestone.HasValues && issueMilestone.Value<string>("title").Equals(milestone)
+                              let category = Categorize(issue.Labels)
+                              where !String.IsNullOrEmpty(category)
                               group issue by category into g
                               select g;
 
-            Console.WriteLine("Found ({0}) issues in {1}", issueGroups.Sum(g => g.Count()), milestone);
+            Console.WriteLine(String.Format("Found ({0}) issues in {1}", issueGroups.Sum(g => g.Count()), milestone.Title));
             Console.WriteLine();
 
+            StringBuilder sb = new StringBuilder();
             foreach (var g in issueGroups)
             {
                 if (String.IsNullOrEmpty(g.Key))
@@ -65,211 +166,44 @@ namespace ReleaseNotesMaker
                     continue;
                 }
 
-                Console.WriteLine("### {0}", g.Key);
-                Console.WriteLine();
+                sb.AppendLine(String.Format("### {0}", g.Key));
+                sb.AppendLine();
 
                 foreach (var issue in g)
                 {
-                    Console.WriteLine("* {0} ([#{1}]({2}))", issue["title"], issue["number"], issue["html_url"]);
+                    sb.AppendLine(String.Format("* {0} ([#{1}]({2}))", issue.Title, issue.Number, issue.HtmlUrl));
                 }
 
-                Console.WriteLine();
+                sb.AppendLine();
             }
+
+            string releaseNotes = sb.ToString();
+            Console.WriteLine(releaseNotes);
+            return releaseNotes;
         }
 
-        private static string Categorize(IEnumerable<string> labels)
+        private static async Task<IReadOnlyList<Issue>> GetClosedIssuesInMilestone(string owner, string repoName, Milestone milestone)
         {
-            if (labels.Any(l => l.Contains("Done")))
+            var issueRequest = new RepositoryIssueRequest() { State = ItemState.Closed, Milestone = milestone.Number.ToString() };
+            var issues = await client.Issue.GetForRepository(owner, repoName, issueRequest);
+            return issues;
+        }
+
+        private static string Categorize(IEnumerable<Label> labels)
+        {
+            if (labels.Any(l => l.Name.Contains("Done")))
             {
-                if (labels.Contains("bug"))
+                if (labels.Any(l => l.Name.Contains("bug")))
                 {
                     return "Bugs Fixed";
                 }
-
-                if (labels.Contains("feature") || labels.Contains("enhancement"))
+                if (labels.Any(l => l.Name.Contains("feature")) || labels.Any(l => l.Name.Contains("enhancement")))
                 {
                     return "Features";
                 }
             }
 
             return String.Empty;
-        }
-
-        private static async Task<List<JToken>> GetIssuesForMilestone(string user, string password, string repoURL)
-        {
-            // TODO: Make this smarter (have it pull updates from github and update issues.json)
-            var issues = new List<JToken>();
-            var cacheFile = MakeCacheFile(repoURL);
-
-            var fi = new FileInfo(cacheFile);
-
-            if (!fi.Exists ||
-                DateTime.UtcNow.Subtract(fi.LastWriteTimeUtc) > TimeSpan.FromDays(1))
-            {
-                var parameters = new Dictionary<string, string>
-                {
-                    { "state", "closed" },
-                    { "per_page", "100" }
-                };
-
-                issues.AddRange(await GetIssues(user, password, repoURL, parameters));
-
-                if (issues.Count > 0)
-                {
-                    File.WriteAllText(cacheFile, JsonConvert.SerializeObject(issues));
-
-                    Console.WriteLine("Wrote ({0}) issues to issues.json", issues.Count);
-                }
-            }
-            else
-            {
-                issues.AddRange(JsonConvert.DeserializeObject<List<JToken>>(File.ReadAllText(cacheFile)));
-
-                Console.WriteLine("Read ({0}) issues from issues.json", issues.Count);
-            }
-
-            return issues;
-        }
-
-        private static string MakeCacheFile(string repoURL)
-        {
-            string s = repoURL;
-            foreach (var ch in Path.GetInvalidFileNameChars())
-            {
-                s = s.Replace(ch, '_');
-            }
-            return s + "_issues.json";
-        }
-
-        private static async Task<IList<JToken>> GetIssues(string user, string password, string repoURL, Dictionary<string, string> parameters = null)
-        {
-            var httpClient = new HttpClient();
-            httpClient.SetBasicAuthCredentials(user, password);
-            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Github Stuff");
-            var issues = new List<JToken>();
-            string resource = AddParameters("https://api.github.com/repos/" + repoURL + "/issues", parameters);
-            string lastResource = null;
-
-            while (true)
-            {
-                Console.WriteLine("GET " + resource);
-                var response = await httpClient.GetAsync(resource);
-
-                if (!response.IsSuccessStatusCode)
-                {
-                    Console.WriteLine(response);
-                    break;
-                }
-                else
-                {
-                    var rateLimitRemainingValue = response.Headers.GetValues("X-RateLimit-Remaining").First();
-                    int rateLimitRemaining;
-                    if (Int32.TryParse(rateLimitRemainingValue, out rateLimitRemaining) && rateLimitRemaining < 10)
-                    {
-                        Console.WriteLine(rateLimitRemainingValue + " requests remaining");
-                    }
-
-                    var pageIssues = JArray.Parse(await response.Content.ReadAsStringAsync());
-                    issues.AddRange(pageIssues);
-
-                    if (resource.Equals(lastResource, StringComparison.OrdinalIgnoreCase))
-                    {
-                        break;
-                    }
-
-                    var resourceLinks = response.GetResourceLinks();
-
-                    if (!resourceLinks.Any())
-                    {
-                        break;
-                    }
-
-                    resource = resourceLinks[0];
-
-                    if (string.IsNullOrEmpty(lastResource))
-                    {
-                        lastResource = resourceLinks[1];
-                    }
-                }
-            }
-
-            return issues;
-        }
-
-        private static string AddParameters(string resource, Dictionary<string, string> parameters)
-        {
-            var builder = new UriBuilder(resource);
-            if (String.IsNullOrEmpty(builder.Query))
-            {
-                builder.Query = BuildQueryString(parameters);
-            }
-            else
-            {
-                builder.Query += "&" + BuildQueryString(parameters);
-            }
-
-            return builder.Uri.ToString();
-        }
-
-        private static string BuildQueryString(Dictionary<string, string> parameters)
-        {
-            var sb = new StringBuilder();
-            bool first = true;
-            foreach (var pair in parameters)
-            {
-                if (!first)
-                {
-                    sb.Append("&");
-                }
-                sb.Append(pair.Key).Append("=").Append(pair.Value);
-                first = false;
-            }
-            return sb.ToString();
-        }
-    }
-
-    public static class HttpClientExtensions
-    {
-        public static void SetBasicAuthCredentials(this HttpClient client, string user, string password)
-        {
-            if (String.IsNullOrEmpty(user) || String.IsNullOrEmpty(password))
-            {
-                return;
-            }
-
-            string base64 = Convert.ToBase64String(Encoding.ASCII.GetBytes(user + ":" + password));
-            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", base64);
-        }
-    }
-
-    public static class HttpResponseMessageExtensions
-    {
-        public static IList<string> GetResourceLinks(this HttpResponseMessage response)
-        {
-            IEnumerable<string> linkHeaderValues;
-            string link = null;
-            if (response.Headers.TryGetValues("Link", out linkHeaderValues))
-            {
-                link = linkHeaderValues.FirstOrDefault();
-            }
-
-            var links = new List<string>();
-
-            if (string.IsNullOrEmpty(link))
-            {
-                return links;
-            }
-
-            foreach (Match match in Regex.Matches(link, "<(.+?)>"))
-            {
-                links.Add(GetResourceUrlFromMatch(match));
-            }
-            return links;
-        }
-
-        private static string GetResourceUrlFromMatch(Match match)
-        {
-            return match.Captures[0].Value.Trim(new[] { '<', '>' });
         }
     }
 }
